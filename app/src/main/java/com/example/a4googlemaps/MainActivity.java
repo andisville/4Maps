7700a9669d6e07b41fc2fdfc41ef2e5d79de3219
Andi <andi_ville@hotmail.com>
2019-03-12 21:25:13 +0100

    Set Intermediate Positions

--- app/src/main/java/com/example/a4googlemaps/MainActivity.java
+++ app/src/main/java/com/example/a4googlemaps/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a4googlemaps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -21,6 +23,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int PERMISSIONS_ACCESS_FINE_LOCATION = 123;
 
     private GoogleMap mMap;
+    private LocationListener l;
+    String p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +50,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_ACCESS_FINE_LOCATION);
         } else {
-            markerDemo();
+            run();
         }
     }
 
@@ -63,13 +62,50 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         if ((requestCode == PERMISSIONS_ACCESS_FINE_LOCATION) &&
                 (grantResults.length > 0 && grantResults[0] ==
                         PackageManager.PERMISSION_GRANTED)) {
-            markerDemo();
+            run();
         }
     }
 
-    private void markerDemo() throws SecurityException {
+    private void run() throws SecurityException {
+
+        l = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                System.out.println("onStatusChanged()\n");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                System.out.println("onProviderEnabled()\n");
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                System.out.println("onProviderDisabled()\n");
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                MarkerOptions options = new MarkerOptions();
+                LatLng newPos = new LatLng(location.getLatitude(), location.getLongitude());
+                options.position(newPos);
+                options.icon(BitmapDescriptorFactory.defaultMarker(
+                        BitmapDescriptorFactory.HUE_YELLOW));
+                mMap.addMarker(options);
+                options.icon(BitmapDescriptorFactory.fromResource(
+                        R.mipmap.ic_launcher));
+            }
+        };
+
         MarkerOptions options = new MarkerOptions();
         LocationManager m = getSystemService(LocationManager.class);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        p = m.getBestProvider(criteria, true);
+
+
+        m.requestLocationUpdates(p, 3000, 0, l);
         Location loc =
                 m.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
         LatLng pos = null;
@@ -98,7 +134,5 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             CameraUpdate cu3 = CameraUpdateFactory.newLatLng(pos);
             mMap.animateCamera(cu3, 5000, null);
         }
-
-
     }
 }
